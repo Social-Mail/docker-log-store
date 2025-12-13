e0075982ddbb7b513b5a6a28b15a30377324e5cd
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-12-13 16:16:27 +0530

    wip

---
+++ log-agent/Program.cs
@@ -0,0 +1,51 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using System.ComponentModel;
+
+namespace DockerLogStoreAgent;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Task.Run(async () =>
+        {
+            try
+            {
+                await MainAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        });
+    }
+
+    static async Task MainAsync()
+    {
+
+        DockerClient client = new DockerClientConfiguration(
+             new Uri("unix:///var/run/docker.sock"))
+            .CreateClient();
+
+
+        var containers = await client.Containers.ListContainersAsync(new Docker.DotNet.Models.ContainersListParameters { All = true });
+
+        foreach (var container in containers) {
+            // watch logs...
+            Task.Run(() => WatchLogs(client, container));
+        }
+
+        // ping...
+        while (true)
+        {
+            await Task.Delay(1000);
+        }
+
+    }
+
+    private static void WatchLogs(DockerClient client, ContainerListResponse container)
+    {
+        container.
+    }
+}
