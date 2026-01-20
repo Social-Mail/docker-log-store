302dbdea37db27078dcab22fa08dc43f5b99bac2
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-01-20 13:06:08 +0530

    wip

--- src/globalEnv.ts
+++ src/globalEnv.ts
@@ -1,7 +1,7 @@
 import { availableParallelism } from "node:os";
 
 
-const isTestMode = (process.env.LOGGER_SERVER_TEST_MODE ?? "false") === "true";
+const isTestMode = (process.env.DOCKER_LOG_STORE_TEST_MODE ?? "false") === "true";
 
 let port = process.env.PORT as any;
 if (/^\d+$/i.test(port)) {
@@ -14,26 +14,26 @@ export const globalEnv = {
     host,
     port,
     isTestMode,
-    adminEmail: process.env.ADMIN_EMAIL,
+    adminEmail: process.env.DOCKER_LOG_STORE_ADMIN_EMAIL,
     smtp: {
-        fromName: process.env.SMTP_FROM_NAME,
-        fromAddress: process.env.SMTP_FROM_ADDRESS,
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT ?? 587,
-        username: process.env.SMTP_USERNAME,
-        password: process.env.SMTP_PASSWORD,
+        fromName: process.env.DOCKER_LOG_STORE_SMTP_FROM_NAME,
+        fromAddress: process.env.DOCKER_LOG_STORE_SMTP_FROM_ADDRESS,
+        host: process.env.DOCKER_LOG_STORE_SMTP_HOST,
+        port: process.env.DOCKER_LOG_STORE_SMTP_PORT ?? 587,
+        username: process.env.DOCKER_LOG_STORE_SMTP_USERNAME,
+        password: process.env.DOCKER_LOG_STORE_SMTP_PASSWORD,
     },
     db: {
-        database: process.env.TRACER_DB_DATABASE ?? "Tracer",
-        host: process.env.TRACER_DB_HOST ?? "localhost",
-        port: Number(process.env.TRACER_DB_PORT ?? 5432),
-        ssl: JSON.parse(process.env.TRACER_DB_SSL ?? "true"),
-        user: process.env.TRACER_DB_USER ?? "postgres",
-        password: process.env.TRACER_DB_PASSWORD ?? "abcd123",
+        database: process.env.DOCKER_LOG_STORE_DB_DATABASE ?? "Tracer",
+        host: process.env.DOCKER_LOG_STORE_DB_HOST ?? "localhost",
+        port: Number(process.env.DOCKER_LOG_STORE_DB_PORT ?? 5432),
+        ssl: JSON.parse(process.env.DOCKER_LOG_STORE_DB_SSL ?? "true"),
+        user: process.env.DOCKER_LOG_STORE_DB_USER ?? "postgres",
+        password: process.env.DOCKER_LOG_STORE_DB_PASSWORD ?? "abcd123",
     },
     numCPUs: isTestMode
         ? 2
-        : (process.env.TRACER_CLUSTER_WORKERS
-            ? Number(process.env.TRACER_CLUSTER_WORKERS)
+        : (process.env.DOCKER_LOG_STORE_CLUSTER_WORKERS
+            ? Number(process.env.DOCKER_LOG_STORE_CLUSTER_WORKERS)
             : availableParallelism())
 };
